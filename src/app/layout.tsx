321adc3b7ab24ab51121848a8304a115a5fe9fea
AadiXC0DE <aadityaz2077@gmail.com>
2026-01-12 20:46:15 +0530

    feat: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -49,6 +51,7 @@ export default function RootLayout({
             <CursorProvider>
                 <CursorEngine />
                 {children}
+                <Analytics />
             </CursorProvider>
           </ThemeProvider>
       </body>
